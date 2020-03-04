200155afa9fcc52e7a8cfa9ba735ebcce020ba20
Marrengula <Marrengula@DESKTOP-0F93IAO>
2020-03-04 12:04:37 +0200

    First Commit haahha

---
+++ public_html/# template/# template/assets/js/demo.js
@@ -0,0 +1,26 @@
+var $instagramSlick = $('.js-br-card-instagram-mobile');
+  $(window).on("orientationchange load resize", function() {
+  var viewPortWidth = $(window).width();
+    if (viewPortWidth < 768) {
+      if (! $instagramSlick.hasClass('slick-initialized')) {
+        $('.js-br-card-instagram-mobile').slick({
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          dots: true,
+          arrows: false,
+          responsive: [
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+            }
+          }
+          ]
+        });
+      }
+    } else {
+      if ($instagramSlick.hasClass('slick-initialized')) {
+        $('.js-br-card-instagram-mobile').slick('destroy');
+      }
+    }
+  )};
